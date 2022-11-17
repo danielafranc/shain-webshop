9615ce7bbdc649411d3a21e69624661bdbb7faac
danielafranc <iosdanielafranco@gmail.com>
2022-11-16 23:45:12 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react"
 import  NavBar  from "./components/NavBar";
 import Hero from "./components/Hero";
-import CardGrid from "./components/CardGrid";
-import CardsComponent from "./components/Cards-component";
 
 function App() {
   return (
